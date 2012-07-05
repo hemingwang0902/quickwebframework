eb9e47ef9890f62006981056088052ef2102c927
scbeta@qq.com <scbeta@qq.com@738baf28-02bd-a08d-2993-4d79d937843c>
2012-07-05 03:41:21 +0000

    MvcFrameworkService类中增加两个方法以得到插件所有的URL

--- examples/qwf.test.core/src/qwf/test/core/controller/IndexController.java
+++ examples/qwf.test.core/src/qwf/test/core/controller/IndexController.java
@@ -1,17 +1,51 @@
 package qwf.test.core.controller;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import javax.servlet.http.HttpServletRequest;
 
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceReference;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.quickwebframework.core.Activator;
+import com.quickwebframework.service.MvcFrameworkService;
+import com.quickwebframework.util.BundleUtil;
 
 @Controller
 public class IndexController {
 
 	@RequestMapping(value = "index", method = RequestMethod.GET)
 	public String index_get(HttpServletRequest request) {
+		BundleContext bundleContext = Activator.getContext();
+		ServiceReference<?> sr = bundleContext
+				.getServiceReference(MvcFrameworkService.class.getName());
+		if (sr == null) {
+			throw new RuntimeException("未找到注册的MvcFrameworkService！");
+		}
+		MvcFrameworkService mvcFrameworkService = (MvcFrameworkService) bundleContext
+				.getService(sr);
 
+		// 得到插件名称与方法名称列表的MAP
+		Map<String, List<String>> map = mvcFrameworkService
+				.getBundleNameMethodNameListMap();
+
+		// 插件名称与URL列表的MAP
+		Map<String, List<String>> bundleNameUrlListMap = new HashMap<String, List<String>>();
+		for (String key : map.keySet()) {
+			List<String> methodNameList = map.get(key);
+			List<String> urlList = new ArrayList<String>();
+			for (String methodName : methodNameList) {
+				urlList.add(BundleUtil.getBundleMethodUrl(key, methodName));
+			}
+			bundleNameUrlListMap.put(key, urlList);
+		}
+		request.setAttribute("bundleNameUrlListMap", bundleNameUrlListMap);
 		return "index";
 	}
 }
