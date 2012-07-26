751ad0a40b977aded9eb7e48991b395f05267865
scbeta@qq.com <scbeta@qq.com@738baf28-02bd-a08d-2993-4d79d937843c>
2012-07-26 02:28:47 +0000

    增加了一个例子用来测试一个Bundle中autowire另一个Bundle中的Component

---
+++ trunk/examples/qwf.test.calculator/src/qwf/test/calculator/Activator.java
@@ -0,0 +1,25 @@
+package qwf.test.calculator;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+import com.quickwebframework.core.FrameworkContext;
+
+public class Activator implements BundleActivator {
+
+	private static BundleContext context;
+
+	static BundleContext getContext() {
+		return context;
+	}
+
+	public void start(BundleContext bundleContext) throws Exception {
+		Activator.context = bundleContext;
+		FrameworkContext.registerWebApp(this, bundleContext);
+	}
+
+	public void stop(BundleContext bundleContext) throws Exception {
+		Activator.context = null;
+	}
+
+}
---
+++ trunk/examples/qwf.test.calculator/src/qwf/test/calculator/controller/DefaultController.java
@@ -0,0 +1,24 @@
+package qwf.test.calculator.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import qwf.test.core.Config;
+
+@Controller
+public class DefaultController {
+
+	@Autowired
+	private Config config;
+
+	@RequestMapping(value = "index", method = RequestMethod.GET)
+	public String index_get(HttpServletRequest request,
+			HttpServletResponse response) {
+		return null;
+	}
+}
