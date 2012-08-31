10ed4a4edc6992f6cdb28fdab8572d5e367363e0
scbeta@qq.com <scbeta@qq.com@738baf28-02bd-a08d-2993-4d79d937843c>
2012-08-31 09:19:00 +0000

    Bundle自动管理线程加入算法根据Bundle的引用计算Bundle的安装顺序。

--- trunk/projects/quickwebframework_web/src/com/quickwebframework/web/thread/BundleAutoManageThread.java
+++ trunk/projects/quickwebframework_web/src/com/quickwebframework/web/thread/BundleAutoManageThread.java
@@ -3,15 +3,20 @@ package com.quickwebframework.web.thread;
 import java.io.File;
 import java.io.FileFilter;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Properties;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipFile;
 
 import org.osgi.framework.Bundle;
 import org.osgi.framework.BundleContext;
+import org.osgi.framework.BundleException;
 import org.osgi.framework.Version;
 
 import com.quickwebframework.web.listener.QuickWebFrameworkLoaderListener;
@@ -21,6 +26,100 @@ public class BundleAutoManageThread extends Thread {
 
 	public BundleAutoManageThread(String bundleFolderPath) {
 		this.bundleFolderPath = bundleFolderPath;
+	}
+
+	private void installOrUpdateBundle(BundleContext bundleContext,
+			BundleInfo bundleInfo) throws BundleException, IOException {
+		String bundleName = bundleInfo.getBundleName();
+		Version bundleVersion = bundleInfo.getBundleVersion();
+		File file = new File(bundleInfo.getBundleFilePath());
+
+		Bundle preBundle = null;
+		Bundle[] bundles = bundleContext.getBundles();
+		for (Bundle bundle : bundles) {
+			if (bundle.getSymbolicName().equals(bundleName)) {
+				preBundle = bundle;
+				break;
+			}
+		}
+
+		Bundle newBundle = null; // 如果之前没有此插件，则安装
+		if (preBundle == null) {
+			System.out.println("自动安装新插件：" + bundleName + "  " + bundleVersion);
+			FileInputStream fileInputStream = new FileInputStream(file);
+			newBundle = bundleContext.installBundle(file.getName(),
+					fileInputStream);
+			fileInputStream.close();
+		}// 否则更新
+		else {
+			if (bundleVersion.compareTo(preBundle.getVersion()) >= 0) {
+				System.out.println("自动将插件：" + bundleName + " 由 "
+						+ preBundle.getVersion() + "更新到" + bundleVersion);
+				FileInputStream fileInputStream = new FileInputStream(file);
+				preBundle.update(fileInputStream);
+				newBundle = preBundle;
+				fileInputStream.close();
+			} else {
+				System.out.println("插件：" + bundleName + "的版本" + bundleVersion
+						+ "小于已安装的版本" + preBundle.getVersion() + "，没有应用更新！");
+			}
+		}
+		// 尝试启动插件
+		if (newBundle != null) {
+			try {
+				newBundle.start();
+			} catch (Exception ex) {
+			}
+		}
+	}
+
+	// 得到Bundle名称列表(主要是为了得到最新的list中的顺序)
+	private List<String> getBundleNameList(List<BundleInfo> list) {
+		List<String> bundleNameList = new ArrayList<String>();
+		for (BundleInfo bundleInfo : list) {
+			bundleNameList.add(bundleInfo.getBundleName());
+		}
+		return bundleNameList;
+	}
+
+	// 根据Bundle的依赖关系，排列出安装顺序
+	private void orderBundleInstallList(List<BundleInfo> list) {
+		// 用于查询
+		List<String> bundleNameList = getBundleNameList(list);
+
+		for (int i = 0; i < list.size();) {
+			// 是否有对象移动
+			boolean isItemMoved = false;
+
+			BundleInfo bundleInfo = list.get(i);
+			for (String requireBundleName : bundleInfo
+					.getRequireBundleNameList()) {
+				// 如果依赖的包不在要安装的插件列表中，则忽略
+				if (!bundleNameList.contains(requireBundleName))
+					continue;
+
+				int requireBundleIndex = bundleNameList
+						.indexOf(requireBundleName);
+				BundleInfo requireBundleInfo = list.get(requireBundleIndex);
+
+				// 如果需要的包在此包后面，则移动到前面
+				if (requireBundleIndex > i) {
+					list.remove(requireBundleIndex);
+					list.add(i, requireBundleInfo);
+
+					bundleNameList = getBundleNameList(list);
+					isItemMoved = true;
+					i++;
+					System.out.println("安装/更新顺序自动计算算法：将插件 " + requireBundleName
+							+ " 移动到 " + bundleInfo.getBundleName() + " 前面。");
+				}
+			}
+			if (isItemMoved) {
+				i = 0;
+			} else {
+				i++;
+			}
+		}
 	}
 
 	@Override
@@ -52,7 +151,7 @@ public class BundleAutoManageThread extends Thread {
 					}
 				});
 
-				List<Bundle> newBundleList = new ArrayList<Bundle>();
+				List<BundleInfo> bundleInfoList = new ArrayList<BundleInfo>();
 				for (File file : files) {
 					ZipFile zipFile = null;
 					try {
@@ -67,61 +166,10 @@ public class BundleAutoManageThread extends Thread {
 							continue;
 						InputStream inputStream = zipFile
 								.getInputStream(zipEntry);
-						Properties prop = new Properties();
-						prop.load(inputStream);
-
-						// 得到插件的名称和版本
-						String bundleName = prop
-								.getProperty("Bundle-SymbolicName");
-						Version bundleVersion = Version.parseVersion(prop
-								.getProperty("Bundle-Version"));
-
-						Bundle preBundle = null;
-						Bundle[] bundles = bundleContext.getBundles();
-						for (Bundle bundle : bundles) {
-							if (bundle.getSymbolicName().equals(bundleName)) {
-								preBundle = bundle;
-								break;
-							}
-						}
-
-						Bundle newBundle = null;
-						// 如果之前没有此插件，则安装
-						if (preBundle == null) {
-							System.out.println("自动安装新插件：" + bundleName + "  "
-									+ bundleVersion);
-							FileInputStream fileInputStream = new FileInputStream(
-									file);
-							newBundle = bundleContext.installBundle(
-									file.getName(), fileInputStream);
-							fileInputStream.close();
-						}// 否则更新
-						else {
-							if (bundleVersion.compareTo(preBundle.getVersion()) >= 0) {
-								System.out.println("自动将插件：" + bundleName
-										+ " 由 " + preBundle.getVersion()
-										+ "更新到" + bundleVersion);
-								FileInputStream fileInputStream = new FileInputStream(
-										file);
-								preBundle.update(fileInputStream);
-								newBundle = preBundle;
-								fileInputStream.close();
-							} else {
-								System.out.println("插件：" + bundleName + "的版本"
-										+ bundleVersion + "小于已安装的版本"
-										+ preBundle.getVersion() + "，没有应用更新！");
-							}
-						}
-						// 尝试启动插件
-						if (newBundle != null) {
-							try {
-								newBundleList.add(newBundle);
-							} catch (Exception ex) {
-							}
-						}
-					} catch (IllegalStateException ex) {
-						ex.printStackTrace();
-						continue;
+						// 得到插件的信息
+						BundleInfo bundleInfo = new BundleInfo(inputStream);
+						bundleInfo.setBundleFilePath(file.getAbsolutePath());
+						bundleInfoList.add(bundleInfo);
 					} catch (Exception ex) {
 						ex.printStackTrace();
 					} finally {
@@ -133,38 +181,18 @@ public class BundleAutoManageThread extends Thread {
 							}
 						}
 					}
+				}
+				// 排出安装顺序
+				orderBundleInstallList(bundleInfoList);
+				// 按照顺序安装并启动
+				for (int i = 0; i < bundleInfoList.size(); i++) {
+					BundleInfo bundleInfo = bundleInfoList.get(i);
+					System.out.println(i + ":" + bundleInfo.getBundleName());
+					installOrUpdateBundle(bundleContext, bundleInfo);
+				}
+				// 删除这些jar文件
+				for (File file : files) {
 					file.delete();
-				}
-
-				// 尝试启动所有的插件
-				boolean isAllBundleStartSuccess;
-				for (int i = 0; i < newBundleList.size(); i++) {
-					isAllBundleStartSuccess = true;
-					for (Bundle newBundle : newBundleList) {
-						if (newBundle.getState() != Bundle.ACTIVE
-								&& newBundle.getState() != Bundle.STARTING) {
-							try {
-								newBundle.start();
-							} catch (Exception e) {
-								isAllBundleStartSuccess = false;
-							}
-						}
-					}
-					if (isAllBundleStartSuccess) {
-						break;
-					}
-				}
-
-				// 启动尝试启动失败的插件以打印异常信息
-				for (Bundle newBundle : newBundleList) {
-					if (newBundle.getState() != Bundle.ACTIVE
-							&& newBundle.getState() != Bundle.STARTING) {
-						try {
-							newBundle.start();
-						} catch (Exception e) {
-							e.printStackTrace();
-						}
-					}
 				}
 			}
 		} catch (InterruptedException e) {
