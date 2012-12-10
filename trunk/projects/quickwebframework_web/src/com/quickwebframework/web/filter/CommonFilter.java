dbfb29e8389189861c98d9699048430f8ae6357a
scbeta@qq.com <scbeta@qq.com@738baf28-02bd-a08d-2993-4d79d937843c>
2012-12-10 03:28:04 +0000

    1.修复第一次运行过滤器未生效的BUG。 2.修复在停止WEB容器时偶尔出现异常的BUG。

--- trunk/projects/quickwebframework_web/src/com/quickwebframework/web/filter/CommonFilter.java
+++ trunk/projects/quickwebframework_web/src/com/quickwebframework/web/filter/CommonFilter.java
@@ -12,36 +12,42 @@ import javax.servlet.ServletResponse;
 import com.quickwebframework.web.listener.QuickWebFrameworkLoaderListener;
 
 public class CommonFilter implements Filter {
-
-	private Filter frameworkBridgeFilter;
 	// QuickwebFramework的过滤器配置状态
 	public static final String QUICKWEBFRAMEWORK_STATE_FILTERCONFIG = "com.quickwebframework.state.FILTERCONFIG";
 
+	private Filter getFrameworkBridgeFilter() {
+		Object frameworkBridgeObject = QuickWebFrameworkLoaderListener
+				.getFrameworkBridgeObject();
+		if (frameworkBridgeObject == null) {
+			return null;
+		}
+		return (Filter) frameworkBridgeObject;
+	}
+
 	@Override
 	public void destroy() {
-		frameworkBridgeFilter.destroy();
+		Filter frameworkBridgeFilter = getFrameworkBridgeFilter();
+		if (frameworkBridgeFilter != null)
+			frameworkBridgeFilter.destroy();
 	}
 
 	@Override
 	public void init(FilterConfig arg0) throws ServletException {
-		Object frameworkBridgeObject = QuickWebFrameworkLoaderListener
-				.getFrameworkBridgeObject();
-		if (frameworkBridgeObject == null) {
+		Filter frameworkBridgeFilter = getFrameworkBridgeFilter();
+		if (frameworkBridgeFilter == null)
 			arg0.getServletContext().setAttribute(
 					QUICKWEBFRAMEWORK_STATE_FILTERCONFIG, arg0);
-		} else {
-			frameworkBridgeFilter = (Filter) frameworkBridgeObject;
+		else
 			frameworkBridgeFilter.init(arg0);
-		}
 	}
 
 	@Override
 	public void doFilter(ServletRequest arg0, ServletResponse arg1,
 			FilterChain arg2) throws IOException, ServletException {
-		if (frameworkBridgeFilter == null) {
+		Filter frameworkBridgeFilter = getFrameworkBridgeFilter();
+		if (frameworkBridgeFilter == null)
 			arg2.doFilter(arg0, arg1);
-			return;
-		}
-		frameworkBridgeFilter.doFilter(arg0, arg1, arg2);
+		else
+			frameworkBridgeFilter.doFilter(arg0, arg1, arg2);
 	}
 }
