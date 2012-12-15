a072801278ac26ac9381cc8efae804f9ef5e2477
scbeta@qq.com <scbeta@qq.com@738baf28-02bd-a08d-2993-4d79d937843c>
2012-12-15 03:57:08 +0000

    修改qwf.test.core插件

---
+++ examples/qwf.test.core/src/qwf/test/core/controller/ControllerInfoController.java
@@ -0,0 +1,30 @@
+package qwf.test.core.controller;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.quickwebframework.entity.HttpMethodInfo;
+import com.quickwebframework.framework.WebContext;
+
+@Controller
+public class ControllerInfoController {
+
+	@RequestMapping(value = "controller", method = RequestMethod.GET)
+	public String get_controller(HttpServletRequest request,
+			HttpServletResponse response) throws IOException {
+		// 得到插件名称与方法名称列表的MAP
+		Map<String, List<HttpMethodInfo>> bundleNameHttpMethodInfoListMap = WebContext
+				.getMvcFrameworkService().getBundleHttpMethodInfoListMap();
+		request.setAttribute("bundleNameHttpMethodInfoListMap",
+				bundleNameHttpMethodInfoListMap);
+		return "controller";
+	}
+}
--- examples/qwf.test.core/src/qwf/test/core/filter/HelloFilter.java
+++ examples/qwf.test.core/src/qwf/test/core/filter/HelloFilter.java
@@ -15,18 +15,17 @@ import org.springframework.stereotype.Component;
 import com.quickwebframework.stereotype.FilterSetting;
 
 @Component
-@FilterSetting(index = 1, returnToController = true)
 public class HelloFilter implements Filter {
 
 	public void destroy() {
 		System.out.println("HelloFilter -> Hello1 -> destroy()");
-		//new RuntimeException().printStackTrace();
+		// new RuntimeException().printStackTrace();
 	}
 
 	public void doFilter(ServletRequest arg0, ServletResponse arg1,
 			FilterChain arg2) throws IOException, ServletException {
-		System.out.println("HelloFilter -> Hello1");
-		return;
+		// System.out.println("HelloFilter -> Hello1");
+		arg2.doFilter(arg0, arg1);
 	}
 
 	public void init(FilterConfig arg0) throws ServletException {
