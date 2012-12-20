ec5f873f458652d7513bd4adcf48a0d2b495fcf6
scbeta@qq.com <scbeta@qq.com@738baf28-02bd-a08d-2993-4d79d937843c>
2012-12-20 02:36:44 +0000

    已加入slf4j的API支持，目前支持的日志器API有：slf4j，Commons Logging，接下来要支持log4j的API

--- trunk/projects/qwf-core/src/org/apache/commons/logging/LogFactory.java
+++ trunk/projects/qwf-core/src/org/apache/commons/logging/LogFactory.java
@@ -1,13 +1,25 @@
 package org.apache.commons.logging;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import com.quickwebframework.framework.LogContext;
+import com.quickwebframework.log.impl.commonslogging.LogImpl;
 
 public abstract class LogFactory {
+	private static Map<String, Log> logMap;
+
 	public static Log getLog(Class<?> clazz) {
-		return (Log) LogContext.getLog(clazz);
+		return getLog(clazz.getName());
 	}
 
 	public static Log getLog(String name) {
-		return (Log) LogContext.getLog(name);
+		if (logMap == null)
+			logMap = new HashMap<String, Log>();
+		if (logMap.containsKey(name))
+			return logMap.get(name);
+		Log log = new LogImpl(LogContext.getLog(name));
+		logMap.put(name, log);
+		return log;
 	}
 }
