1f0f9eade42b10ed13e7cd5ea707b6bf443600e1
scbeta@qq.com <scbeta@qq.com@738baf28-02bd-a08d-2993-4d79d937843c>
2013-01-16 08:53:04 +0000

    1.mvc类型请求调整完成 2.资源访问Servlet添加完成

--- trunk/projects/qwf-mvc/src/com/quickwebframework/mvc/impl/Activator.java
+++ trunk/projects/qwf-mvc/src/com/quickwebframework/mvc/impl/Activator.java
@@ -10,6 +10,7 @@ import com.quickwebframework.mvc.servlet.MvcDispatchServlet;
 public class Activator implements BundleActivator {
 
 	private static BundleContext context;
+	private MvcDispatchServlet mvcDispatchServlet;
 
 	public static BundleContext getContext() {
 		return context;
@@ -18,15 +19,21 @@ public class Activator implements BundleActivator {
 	public void start(BundleContext bundleContext) throws Exception {
 		Activator.context = bundleContext;
 		// 注册视图类型处理Servlet
-		WebContext.registerViewTypeServlet("mvc", new MvcDispatchServlet());
-
+		mvcDispatchServlet = new MvcDispatchServlet();
+		String mvcViewTypeName = mvcDispatchServlet.getViewTypeName();
+		if (mvcViewTypeName == null) {
+			throw new RuntimeException(
+					"未找到配置[qwf.config.qwf-mvc.MvcDispatchServlet.viewTypeName]!");
+		}
+		WebContext.registerViewTypeServlet(mvcViewTypeName, mvcDispatchServlet);
 		MvcContext.getInstance().init();
 	}
 
 	public void stop(BundleContext bundleContext) throws Exception {
 		MvcContext.getInstance().destory();
 		// 取消注册视图类型处理Servlet
-		WebContext.unregisterViewTypeServlet("mvc");
+		WebContext.unregisterViewTypeServlet(mvcDispatchServlet
+				.getViewTypeName());
 		Activator.context = null;
 	}
 }
