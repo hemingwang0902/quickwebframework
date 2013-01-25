71ea693217a29c4937c4db97ee0dd4b770d67d9b
scbeta@qq.com <scbeta@qq.com@738baf28-02bd-a08d-2993-4d79d937843c>
2013-01-25 10:53:28 +0000

    1.公司垮了，年终奖没了。明年在成都高新区找个好工作，希望能进支付宝，将来转到阿里巴巴。 2.为支持Struts2作准备。

---
+++ projects/qwf-view-struts2/src/com/quickwebframework/view/struts2/servlet/PluginStruts2DispatchServlet.java
@@ -0,0 +1,61 @@
+package com.quickwebframework.view.struts2.servlet;
+
+import java.io.IOException;
+import java.util.Enumeration;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.struts2.dispatcher.ng.filter.StrutsPrepareAndExecuteFilter;
+import org.osgi.framework.Bundle;
+
+import com.quickwebframework.framework.WebContext;
+import com.quickwebframework.view.struts2.support.Struts2FilterConfig;
+import com.quickwebframework.view.struts2.support.Struts2ServletContext;
+
+public class PluginStruts2DispatchServlet extends HttpServlet {
+
+	private static final long serialVersionUID = 7768803244477900595L;
+	private Log log = LogFactory.getLog(PluginStruts2DispatchServlet.class);
+	private transient ServletContext context;
+	private ServletConfig servletConfig;
+	private FilterConfig filterConfig;
+	private Struts2ViewTypeServlet struts2ViewTypeServlet;
+	private Bundle bundle;
+	private StrutsPrepareAndExecuteFilter struts2Filter = null;
+
+	public PluginStruts2DispatchServlet(
+			Struts2ViewTypeServlet struts2ViewTypeServlet, Bundle bundle) {
+		this.struts2ViewTypeServlet = struts2ViewTypeServlet;
+		this.bundle = bundle;
+
+		struts2Filter = new StrutsPrepareAndExecuteFilter();
+
+	}
+
+	@Override
+	public void init(final ServletConfig config) throws ServletException {
+		super.init(config);
+		this.servletConfig = config;
+		this.context = new Struts2ServletContext(config.getServletContext());
+		this.filterConfig = new Struts2FilterConfig(this.servletConfig,
+				this.context, bundle);
+		struts2Filter.init(this.filterConfig);
+	}
+
+	@Override
+	public void service(HttpServletRequest request, HttpServletResponse response)
+			throws IOException, ServletException {
+		String pathName = request.getAttribute(WebContext.CONST_PATH_NAME)
+				.toString();
+		struts2Filter.doFilter(request, response, null);
+	}
+}
---
+++ projects/qwf-view-struts2/src/com/quickwebframework/view/struts2/servlet/Struts2ViewTypeServlet.java
@@ -0,0 +1,91 @@
+package com.quickwebframework.view.struts2.servlet;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.osgi.framework.Bundle;
+import org.osgi.framework.BundleEvent;
+import org.osgi.framework.BundleListener;
+
+import com.quickwebframework.framework.OsgiContext;
+import com.quickwebframework.framework.WebContext;
+import com.quickwebframework.servlet.ViewTypeServlet;
+import com.quickwebframework.view.struts2.support.Activator;
+
+public class Struts2ViewTypeServlet extends ViewTypeServlet {
+
+	private static final long serialVersionUID = 830203994209104651L;
+	public static final String VIEW_TYPE_NAME_PROPERTY_KEY = "qwf-view-struts2.Struts2ViewTypeServlet.viewTypeName";
+
+	private Map<String, PluginStruts2DispatchServlet> pluginNameServletMap = new HashMap<String, PluginStruts2DispatchServlet>();
+	private BundleListener bundleListener;
+	private ServletConfig config;
+
+	public Struts2ViewTypeServlet(String viewTypeName) {
+		super(viewTypeName);
+
+		bundleListener = new BundleListener() {
+			@Override
+			public void bundleChanged(BundleEvent event) {
+				Bundle bundle = event.getBundle();
+				String pluginName = bundle.getSymbolicName();
+				if (BundleEvent.STOPPING == event.getType()) {
+					if (pluginNameServletMap.containsKey(pluginName)) {
+						pluginNameServletMap.remove(pluginName);
+					}
+				}
+			}
+		};
+	}
+
+	private PluginStruts2DispatchServlet createNewPluginStruts2DispatchServlet(
+			Bundle bundle) {
+		PluginStruts2DispatchServlet servlet = new PluginStruts2DispatchServlet(
+				this, bundle);
+		try {
+			servlet.init(config);
+		} catch (ServletException e) {
+			throw new RuntimeException(e);
+		}
+		return servlet;
+	}
+
+	@Override
+	public String[] getUrls() {
+		return null;
+	}
+
+	@Override
+	public void init(final ServletConfig config) throws ServletException {
+		super.init(config);
+		this.config = config;
+		Activator.getContext().addBundleListener(bundleListener);
+	}
+
+	@Override
+	public void destroy() {
+		super.destroy();
+		Activator.getContext().removeBundleListener(bundleListener);
+	}
+
+	@Override
+	public void service(HttpServletRequest request, HttpServletResponse response)
+			throws IOException, ServletException {
+		String pluginName = request.getAttribute(WebContext.CONST_PLUGIN_NAME)
+				.toString();
+		PluginStruts2DispatchServlet pluginStruts2DispatchServlet = pluginNameServletMap
+				.get(pluginName);
+		if (pluginStruts2DispatchServlet == null) {
+			Bundle bundle = OsgiContext.getBundleByName(pluginName);
+			pluginStruts2DispatchServlet = createNewPluginStruts2DispatchServlet(bundle);
+			pluginNameServletMap.put(pluginName, pluginStruts2DispatchServlet);
+		}
+		pluginStruts2DispatchServlet.service(request, response);
+	}
+}
