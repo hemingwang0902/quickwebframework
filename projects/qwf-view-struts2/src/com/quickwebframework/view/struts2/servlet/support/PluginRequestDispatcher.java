75e8bf7fa65af5d77bda1c9cfb8d51681307b631
scbeta@qq.com <scbeta@qq.com@738baf28-02bd-a08d-2993-4d79d937843c>
2013-01-27 08:24:08 +0000

    Struts2的Action处理部分已经搞定，现在还差视图的渲染。

---
+++ projects/qwf-view-struts2/src/com/quickwebframework/view/struts2/servlet/support/PluginRequestDispatcher.java
@@ -0,0 +1,42 @@
+package com.quickwebframework.view.struts2.servlet.support;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.URL;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.io.IOUtils;
+import org.osgi.framework.Bundle;
+
+public class PluginRequestDispatcher implements RequestDispatcher {
+
+	private URL url;
+
+	public PluginRequestDispatcher(URL url) {
+		this.url = url;
+	}
+
+	@Override
+	public void forward(ServletRequest arg0, ServletResponse arg1)
+			throws ServletException, IOException {
+		HttpServletResponse response = (HttpServletResponse) arg1;
+		InputStream input = url.openStream();
+		OutputStream output = response.getOutputStream();
+		IOUtils.copy(input, output);
+		output.flush();
+		output.close();
+		input.close();
+	}
+
+	@Override
+	public void include(ServletRequest arg0, ServletResponse arg1)
+			throws ServletException, IOException {
+		throw new ServletException("此方法还未实现！");
+	}
+}
